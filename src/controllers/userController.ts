610a69eaefd4d948fbe852bb47c75931d46a190b
MaykonWSF <maykonwillyam038@gmail.com>
2024-09-11 16:23:25 -0300

    Rota para buscar por usuário por ID

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -10,3 +10,22 @@ export const getAllUsers = async (req: Request, res: Response) => {
         return res.status(500).json({ error: 'Erro ao buscar usuários.' });
     }
 };
+
+export const getUserById = async (req: Request, res: Response) => {
+    const { id } = req.params; // Pega o id da URL
+
+    try {
+        const user = await prisma.usuario.findUnique({
+            where: { userId: Number(id) },
+        });
+
+        if (!user) {
+            return res.status(404).json({ error: 'Usuário não encontrado' });
+        }
+
+        return res.status(200).json(user);
+    } catch (error) {
+        console.error('Erro ao buscar usuário:', error);
+        return res.status(500).json({ error: 'Erro ao buscar o usuário' });
+    }
+};
--- src/routes/user.ts
+++ src/routes/user.ts
@@ -1,10 +1,11 @@
 import express from 'express';
 import { updateUser } from '../controllers/updateUserController';
-import { getAllUsers } from '../controllers/userController';
+import { getAllUsers, getUserById } from '../controllers/userController';
 
 const router = express.Router();
 
 router.put('/user/update/:id', updateUser); 
 router.get('/users', getAllUsers);
+router.get('/users/:id', getUserById);
 
 export default router;
