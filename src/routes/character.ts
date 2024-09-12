a090a1f61a445bdc17eb4837863bfd833af4d21a
MaykonWSF <maykonwillyam038@gmail.com>
2024-09-12 02:44:38 -0300

    Implementando rota de busca de personagem por ID

--- src/routes/character.ts
+++ src/routes/character.ts
@@ -1,9 +1,9 @@
 import express from 'express';
-import { addCharacter } from '../controllers/characterController';
+import { /*addCharacter,*/ getCharacterById } from '../controllers/characterController';
 
 const router = express.Router();
 
-router.post('/character/insert', addCharacter);
-
+//router.post('/character/insert', addCharacter);
+router.get('/character/:id', getCharacterById);
 
 export default router;
