5382405c6e68b8b1f03529bc57a9e7c2b9251633
avelando <avelarrodrigues89@gmail.com>
2024-09-20 01:57:24 -0300

    Tinha esquecido a rota

--- src/routes/character.ts
+++ src/routes/character.ts
@@ -1,9 +1,10 @@
 import express from 'express';
-import { getCharacter, uploadCharacter, deleteCharacter, updateCharacter } from '../controllers/characterController';
+import { getCharacter, uploadCharacter, deleteCharacter, updateCharacter, getAllCharacters } from '../controllers/characterController';
 
 const router = express.Router();
 
 router.get('/character/', getCharacter);
+router.get('/character/all', getAllCharacters);
 router.post('/character', uploadCharacter);
 router.delete('/character/:id', deleteCharacter);
 router.put('/character/:id', updateCharacter);
