8ffb4639286c52ad22b4f390c7cd3785ffdfcb2d
avelando <avelarrodrigues89@gmail.com>
2024-09-20 15:54:29 -0300

    Again

--- src/app.ts
+++ src/app.ts
@@ -15,7 +15,6 @@ app.use(express.json());
 app.use(cors({
   origin: 'https://wordleofthrones.vercel.app/',
   methods: 'GET,POST,PUT,DELETE',
-  credentials: true,
   allowedHeaders: 'Content-Type,Authorization',
 }));
 
