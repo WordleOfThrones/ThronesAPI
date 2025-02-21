ec0c861b7b36a2078325abb8ff34560557da3e48
MaykonWSF <maykonwillyam038@gmail.com>
2025-02-21 16:51:54 -0300

    Adicionando rota de logout

--- src/routes/user.ts
+++ src/routes/user.ts
@@ -1,5 +1,5 @@
 import express from 'express';
-import { updateUser, loginUser, getUserById, registerUser, getAllUsers } from '../controllers/userController';
+import { updateUser, loginUser, logoutUser, getUserById, registerUser, getAllUsers } from '../controllers/userController';
 import { authenticateToken } from '../middleware/authMiddleware';
 
 const router = express.Router();
@@ -9,5 +9,6 @@ router.get('/user', authenticateToken, getAllUsers);
 router.get('/user/:id', authenticateToken, getUserById);
 router.post('/user/register', registerUser);
 router.post('/user/login', loginUser);
+router.post('/user/logout', authenticateToken, logoutUser);
 
 export default router;
