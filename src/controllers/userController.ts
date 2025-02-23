f48e2d552cdf9bae0cc55cf8ece8736993cd25f3
MaykonWSF <maykonwillyam038@gmail.com>
2025-02-23 19:09:40 -0300

    Teste de cookies

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -28,8 +28,8 @@ export const loginUser = async (req: Request, res: Response) => {
 
     res.cookie("token", token, {
       httpOnly: true,
-      secure: process.env.NODE_ENV === "production",
-      sameSite: "lax",
+      secure: true,
+      sameSite: "none",
       maxAge: 2 * 60 * 60 * 1000, // Expira em 2 horas
     });
 
