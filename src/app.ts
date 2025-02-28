02c68da1fe80977a15b0f20312fa18e0005adf61
MaykonWSF <maykonwillyam038@gmail.com>
2025-02-28 13:35:23 -0300

    Alterando horário do schedule

--- src/app.ts
+++ src/app.ts
@@ -34,7 +34,7 @@ app.use(cors({
   allowedHeaders: ['Content-Type', 'Authorization'],
 }));
 
-cron.schedule("0 0 * * *", async () => {
+cron.schedule("5 0 * * *", async () => {
   console.log("Executando inserção diária de personagens...");
 
   try {
