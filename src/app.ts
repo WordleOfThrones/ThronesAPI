297e82cd02e01ad057f0df3c8f9440d4403baec8
CarlHenry670 <capic.2023118tads0035@aluno.ifpi.edu.br>
2025-03-27 17:20:55 -0300

    testando

--- src/app.ts
+++ src/app.ts
@@ -19,7 +19,9 @@ const allowedOrigins = [
   "https://wordleofthrones.vercel.app",
   "https://wordleofthrones-nn604k8ws-avelar-rodrigues-de-sousas-projects.vercel.app",
   "http://localhost:3000",
-  "https://wordle-of-thrones-projeto.vercel.app"
+  "https://wordle-of-thrones-projeto.vercel.app",
+  "https://wordle-of-thrones-4iwq-git-feature-carls-projects-c602a40b.vercel.app",
+  "https://wordle-of-thrones-qdyvs6by3-carls-projects-c602a40b.vercel.app",
 ];
 
 app.use(
